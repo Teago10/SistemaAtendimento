7ebe7d82f87cf20f258897029c223a1b9858b474
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-09-03 22:23:23 -0300

    Criação do Listar do Situação Atendimento

---
+++ Repositories/SituacaoAtendimentoRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaAtendimento.Database;
+using SistemaAtendimento.Model;
+
+namespace SistemaAtendimento.Repositories
+{
+    public class SituacaoAtendimentoRepository
+    {
+        public List<SituacaoAtendimentos> Listar()
+        {
+            var situacaoAtendimentos = new List<SituacaoAtendimentos>();
+            
+            using (var conexao = ConexaoDB.GetConexao()
+            {
+                string sql = "select * from situacaoatendimentos";
+            }
+            return situacaoAtendimentos;
+        }
+    }
+}
